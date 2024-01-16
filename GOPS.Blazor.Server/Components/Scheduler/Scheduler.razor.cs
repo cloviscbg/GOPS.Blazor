143c39c5d93c811e40b6aac9ae7fb9a4820ca453
cloviscbg <cloviscbg@gmail.com>
2024-01-15 23:53:54 -0100

    Added Datepicker

--- GOPS.Blazor.Server/Components/Scheduler/Scheduler.razor.cs
+++ GOPS.Blazor.Server/Components/Scheduler/Scheduler.razor.cs
@@ -6,10 +6,12 @@ using GOPS.Client.Shared.Utils;
 using GOPS.Client.Shared.Extensions;
 using GOPS.Client.Shared.Services;
 using GOPS.Blazor.Server.Components.Internal;
+using ZXing;
 
 namespace GOPS.Blazor.Server.Components;
 public partial class Scheduler
 {
+	DateTime? currentDate;
 	int columnCount;
 	bool groupsExpanded;
 	bool fixedHeader;
@@ -55,15 +57,40 @@ public partial class Scheduler
 	[Inject]
 	public IShiftService ShiftService { get; set; } = default!;
 
-	public DateTime CurrentDate { get; private set; }
 	public int DefaultMinutesInterval { get; private set; }
-	public string CurrentDateText { get; private set; } = "";
+	public string CurrentDateFormat { get; set; } = string.Empty;
+	public string CurrentDateText { get; set; } = string.Empty;
 	public ViewType? CurrentView { get; private set; }
 	public List<DayCell>? DayCells { get; set; }
 	public List<Note>? Notes { get; set; }
 	public List<Group>? Groups { get; set; }
 	public List<People>? Peoples { get; set; }
 	public List<Shift>? Shifts { get; set; }
+
+	public DateTime? CurrentDate
+	{
+		get { return currentDate; }
+		set
+		{
+			if (!currentDate.HasValue)
+			{
+				currentDate = value;
+				UpdateView();
+				return;
+			}
+
+			//if (value!.Value.IsDateEqual(currentDate!.Value))
+			//	return;
+
+			if (CheckDate(value))
+				return;
+
+			currentDate = value;
+			UpdateView();
+			UpdateData();
+			StateHasChanged();
+		}
+	}
 
 	public int ColumnCount
 	{
@@ -178,15 +205,9 @@ public partial class Scheduler
 		UpdateData();
 	}
 
-	public void SetDate(DateTime date)
+	public void SetDate(DateTime? date)
 	{
-		if (date.IsDateEqual(CurrentDate))
-			return;
-
 		CurrentDate = date;
-
-		UpdateView();
-		UpdateData();
 	}
 
 	public void OffsetDate(int offset)
@@ -195,12 +216,23 @@ public partial class Scheduler
 
 		date = CurrentView switch
 		{
-			ViewType.DayView => date.AddDays(offset),
-			ViewType.WeekView => date.AddDays(offset * 7),
-			_ => date.AddMonths(offset),
+			ViewType.DayView => date?.AddDays(offset),
+			ViewType.WeekView => date?.AddDays(offset * 7),
+			_ => date?.AddMonths(offset),
 		};
 
 		SetDate(date);
+	}
+
+	bool CheckDate(DateTime? date)
+	{
+		return CurrentView switch
+		{
+			ViewType.DayView => date!.Value.IsDateEqual(currentDate!.Value),
+			ViewType.WeekView => date!.Value.GetWeekNumber() == currentDate!.Value.GetWeekNumber(),
+			ViewType.MonthView => currentDate!.Value.Month == date!.Value.Month && currentDate!.Value.Year == date.Value.Year,
+			_ => false
+		};
 	}
 
 	void NoteChange(Note note)
@@ -219,17 +251,20 @@ public partial class Scheduler
 			case ViewType.DayView:
 				//Get the total minutes of 24h and then divide for the desired DefaultMinutesInterval to get the total column count of the grid;
 				columnCount = 1440 / DefaultMinutesInterval;
-				CurrentDateText = CurrentDate.ToString("ddd, dd MMM yy").Capitalize();
+				CurrentDateFormat = "ddd, dd MMM yy";
+				CurrentDateText = CurrentDate?.ToString("ddd, dd MMM yy").Capitalize()!;
 				break;
 			case ViewType.WeekView:
 				columnCount = 7;
-				DayCells = CurrentDate.FromFirstDayOfWeek().CreateDayCells(columnCount).ToList();
-				CurrentDateText = CurrentDate.ToString("dd MMM yyyy").Capitalize();
+				DayCells = CurrentDate?.FromFirstDayOfWeek().CreateDayCells(columnCount).ToList();
+				CurrentDateFormat = "dd MMM yyyy";
+				CurrentDateText = CurrentDate?.ToString("dd MMM yyyy").Capitalize()!;
 				break;
 			case ViewType.MonthView:
-				columnCount = CurrentDate.EndOfMonth().Day;
-				DayCells = CurrentDate.StartOfMonth().CreateDayCells(columnCount).ToList();
-				CurrentDateText = CurrentDate.ToString("MMMM yyyy").Capitalize();
+				columnCount = CurrentDate!.Value.EndOfMonth().Day;
+				DayCells = CurrentDate?.StartOfMonth().CreateDayCells(columnCount).ToList();
+				CurrentDateFormat = "MMMM yyyy";
+				CurrentDateText = CurrentDate?.ToString("MMMM yyyy").Capitalize()!;
 				break;
 			default:
 				break;
@@ -242,10 +277,10 @@ public partial class Scheduler
 		Groups?.Clear();
 		Peoples?.Clear();
 		Shifts?.Clear();
-		Notes = NotesService.Get(CurrentDate).ToList();
+		Notes = NotesService.Get(CurrentDate!.Value).ToList();
 		Groups = GroupsService.Get().ToList();
 		Peoples = PeopleService.Get().ToList();
-		Shifts = ShiftService.GetAllOfMonth(CurrentDate).ToList();
+		Shifts = ShiftService.GetAllOfMonth(CurrentDate!.Value).ToList();
 	}
 
 	public IEnumerable<Shift> GetShiftsBetweenDates(Guid? peopleId = null)
@@ -257,14 +292,14 @@ public partial class Scheduler
 			if (peopleId is not null)
 			{
 				foundShifts = Shifts?
-					.Where(x => x.PeopleId == peopleId && x.StartDate.IsDateEqual(CurrentDate))
+					.Where(x => x.PeopleId == peopleId && x.StartDate.IsDateEqual(CurrentDate!.Value))
 					.ToList() ?? [];
 
 				return foundShifts;
 			}
 
 			foundShifts = Shifts?
-				.Where(x => x.StartDate.IsDateEqual(CurrentDate))
+				.Where(x => x.StartDate.IsDateEqual(CurrentDate!.Value))
 				.ToList() ?? [];
 
 			return foundShifts;
@@ -331,8 +366,8 @@ public partial class Scheduler
 
 			var dateToCheck = CurrentDate + TimeSpan.FromHours(hour);
 
-			count = GetShiftsByDate(CurrentDate)
-				.Count(s => dateToCheck.IsBetween(s.StartDate, s.EndDate.AddHours(-1)));
+			count = GetShiftsByDate(CurrentDate!.Value)
+				.Count(s => dateToCheck!.Value.IsBetween(s.StartDate, s.EndDate.AddHours(-1)));
 
 			return count;
 		}
